2fe4c1f06db7b71bf6ca00456336c0661a85b386
danjarosz <jarosz.daniel@protonmail.com>
2021-07-31 12:17:28 +0200

    feat(Project JS&DOM): search elements

--- JS&DOM/main.js
+++ JS&DOM/main.js
@@ -1,4 +1,5 @@
 const contentEl = document.querySelector(".content");
+const resultEl = document.querySelector(".result");
 
 const addElement = (
   node = "div",
@@ -8,7 +9,7 @@ const addElement = (
   destination = ""
 ) => {
   if (!destination) {
-    return;
+    throw new Error("parameter destination is required");
   }
   const element = document.createElement(node);
   const textNode = document.createTextNode(txt);
@@ -57,3 +58,122 @@ const handleAddElementSubmit = (e, destination) => {
 };
 
 addForm.addEventListener("submit", (e) => handleAddElementSubmit(e, contentEl));
+
+// Search form
+const searchForm = document.querySelector(".form--search");
+
+const CreateElementSummary = (
+  tagName,
+  classAttr,
+  offsetHeight,
+  offsetWidth,
+  offsetLeft,
+  offsetTop,
+  childNodesAmount
+) => {
+  const element = document.createElement("div");
+
+  const tagNameEl = document.createElement("p");
+  const tagNameTextNode = document.createTextNode(`Tag: ${tagName}`);
+  tagNameEl.appendChild(tagNameTextNode);
+
+  const classAttrEl = document.createElement("p");
+  const classAttrtextNode = document.createTextNode(
+    `Class / Classes: ${classAttr}`
+  );
+  classAttrEl.appendChild(classAttrtextNode);
+
+  const offsetHeightEl = document.createElement("p");
+  const offsetHeightTextNode = document.createTextNode(
+    `Wysokość elementu (offsetHeight): ${offsetHeight}`
+  );
+  offsetHeightEl.appendChild(offsetHeightTextNode);
+
+  const offsetWidthEl = document.createElement("p");
+  const offsetWidthTextNode = document.createTextNode(
+    `Szerokość elementu(offsetWidth): ${offsetWidth}`
+  );
+  offsetWidthEl.appendChild(offsetWidthTextNode);
+
+  const offsetLeftEl = document.createElement("p");
+  const offsetLeftTextNode = document.createTextNode(
+    `Odległość od lewej krawędzi (offsetLeft): ${offsetLeft}`
+  );
+  offsetLeftEl.appendChild(offsetLeftTextNode);
+
+  const offsetTopEl = document.createElement("p");
+  const offsetTopTextNode = document.createTextNode(
+    `Odległość od górnej krawędzi (offsetTop): ${offsetTop}`
+  );
+  offsetTopEl.appendChild(offsetTopTextNode);
+
+  const childNodesAmountEl = document.createElement("p");
+  const childNodesAmountTextNode = document.createTextNode(
+    `Liczba elementów dzieci: ${childNodesAmount}`
+  );
+  childNodesAmountEl.appendChild(childNodesAmountTextNode);
+
+  element.appendChild(tagNameEl);
+  element.appendChild(classAttrEl);
+  element.appendChild(offsetHeightEl);
+  element.appendChild(offsetWidthEl);
+  element.appendChild(offsetLeftEl);
+  element.appendChild(offsetTopEl);
+  element.appendChild(childNodesAmountEl);
+
+  return element;
+};
+
+const handleSearchFormSubmit = (e, resultTarget) => {
+  e.preventDefault();
+
+  if (!resultTarget) {
+    throw new Error("resultTarged is required");
+  }
+
+  resultTarget.innerHTML = "";
+
+  const formEl = e.target;
+  const searchInput = formEl.querySelector('[name="search"]');
+  const searchInputValue = searchInput.value;
+
+  if (!searchInput) {
+    return;
+  }
+
+  const foundNodes = [...document.querySelectorAll(searchInputValue)];
+
+  if (foundNodes.length === 0) {
+    const node = "p";
+    const text = `Nie znaleziono elementów: ${searchInputValue}`;
+    const style = "flex-basis: 100%; text-align: center";
+    addElement(node, text, "style", style, resultTarget);
+    return;
+  }
+
+  foundNodes.forEach((foundNode) => {
+    const tagName = foundNode.tagName;
+    const classAttr = foundNode.getAttribute("class");
+    const offsetHeight = foundNode.offsetHeight;
+    const offsetWidth = foundNode.offsetWidth;
+    const offsetLeft = foundNode.offsetLeft;
+    const offsetTop = foundNode.offsetTop;
+    const childNodesAmount = foundNode.childElementCount;
+
+    const element = CreateElementSummary(
+      tagName,
+      classAttr,
+      offsetHeight,
+      offsetWidth,
+      offsetLeft,
+      offsetTop,
+      childNodesAmount
+    );
+
+    resultTarget.appendChild(element);
+  });
+};
+
+searchForm.addEventListener("submit", (e) =>
+  handleSearchFormSubmit(e, resultEl)
+);
