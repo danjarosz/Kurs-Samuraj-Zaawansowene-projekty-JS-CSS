6b77b5ab6d2567d0d05448f2fbc474d4fa7e75a8
danjarosz <jarosz.daniel@protonmail.com>
2021-08-09 19:38:56 +0200

    feat: scroll

--- Projects/Scroller/index.js
+++ Projects/Scroller/index.js
@@ -1,8 +1,37 @@
 document.addEventListener("DOMContentLoaded", () => {
   const rootElement = document.querySelector("#root");
   const sections = [...rootElement.querySelectorAll("section")];
+  let currentSectionIndex = 0;
+  let isThrottled = false;
 
   document.addEventListener("mousewheel", (event) => {
-    console.log(event.wheelDelta);
+    if (isThrottled) {
+      return;
+    }
+    isThrottled = true;
+
+    setTimeout(() => {
+      isThrottled = false;
+    }, 1000);
+
+    const direction = event.wheelDelta < 0 ? 1 : -1;
+
+    if (direction === 1) {
+      const isLastSection = currentSectionIndex === sections.length - 1;
+      if (isLastSection) {
+        return;
+      }
+    } else if (direction === -1) {
+      const isFirstSection = currentSectionIndex === 0;
+      if (isFirstSection) {
+        return;
+      }
+    }
+
+    currentSectionIndex = currentSectionIndex + direction;
+    sections[currentSectionIndex].scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
   });
 });
