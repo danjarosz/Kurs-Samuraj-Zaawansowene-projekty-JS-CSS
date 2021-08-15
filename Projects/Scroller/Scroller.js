bc04407898f195c8df6510fe014f1c581996fc95
danjarosz <jarosz.daniel@protonmail.com>
2021-08-15 20:22:18 +0200

    feat: implement navigation

--- Projects/Scroller/Scroller.js
+++ Projects/Scroller/Scroller.js
@@ -7,6 +7,8 @@ class Scroller {
     );
     this.currentSectionIndex = visibleSection < 0 ? 0 : visibleSection;
     this.isThrottled = false;
+
+    this.drawNavigation();
   }
 
   isScrolledIntoView = (el) => {
@@ -33,6 +35,7 @@ class Scroller {
   };
 
   scrollToCurrentSection = () => {
+    this.selectActiveNavItem();
     this.sections[this.currentSectionIndex].scrollIntoView({
       behavior: "smooth",
       block: "start",
@@ -55,5 +58,41 @@ class Scroller {
 
     this.currentSectionIndex = this.currentSectionIndex + direction;
     this.scrollToCurrentSection();
+  };
+
+  drawNavigation = () => {
+    this.navigationContainer = document.createElement("aside");
+    this.navigationContainer.setAttribute("class", "scroller__navigation");
+    const list = document.createElement("ul");
+
+    this.sections.forEach((section, index) => {
+      const listItem = document.createElement("li");
+
+      listItem.addEventListener("click", () => {
+        this.currentSectionIndex = index;
+        this.scrollToCurrentSection();
+      });
+
+      list.appendChild(listItem);
+    });
+
+    this.navigationContainer.appendChild(list);
+    document.body.appendChild(this.navigationContainer);
+    this.selectActiveNavItem();
+  };
+
+  selectActiveNavItem = () => {
+    if (!this.navigationContainer) {
+      return;
+    }
+
+    const navigationItems = this.navigationContainer.querySelectorAll("li");
+    navigationItems.forEach((item, index) => {
+      if (index === this.currentSectionIndex) {
+        item.classList.add("active");
+      } else {
+        item.classList.remove("active");
+      }
+    });
   };
 }
