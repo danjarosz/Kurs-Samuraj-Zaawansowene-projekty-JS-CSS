19893467583e307352c80bd69f9cbdd6ea334e96
danjarosz <jarosz.daniel@protonmail.com>
2021-08-15 22:38:26 +0200

    feat: swipe

--- Projects/Scroller/index.js
+++ Projects/Scroller/index.js
@@ -3,4 +3,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const scroller = new Scroller(rootElement);
 
   document.addEventListener("wheel", scroller.listenScroll);
+
+  document.addEventListener("swipeUp", () => scroller.scroll(1));
+  document.addEventListener("swipeDown", () => scroller.scroll(-1));
 });
