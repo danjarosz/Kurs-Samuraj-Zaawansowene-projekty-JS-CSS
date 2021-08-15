8585f0ecb77f7916f00801ff36b4cfbf4f074909
danjarosz <jarosz.daniel@protonmail.com>
2021-08-15 22:45:12 +0200

    feat: support keyboard usage

--- Projects/Scroller/index.js
+++ Projects/Scroller/index.js
@@ -3,7 +3,16 @@ document.addEventListener("DOMContentLoaded", () => {
   const scroller = new Scroller(rootElement);
 
   document.addEventListener("wheel", scroller.listenScroll);
-
+  document.addEventListener("keydown", (event) => {
+    switch (event.keyCode) {
+      case 40:
+        return scroller.scroll(1);
+      case 38:
+        return scroller.scroll(-1);
+      default:
+        return;
+    }
+  });
   document.addEventListener("swipeUp", () => scroller.scroll(1));
   document.addEventListener("swipeDown", () => scroller.scroll(-1));
 });
