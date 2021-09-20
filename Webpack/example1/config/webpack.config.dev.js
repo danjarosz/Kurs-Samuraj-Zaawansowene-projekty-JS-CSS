cc2ba6e2e003fa4c8d2444a50b9e9f3b2e44a3d8
danjarosz <jarosz.daniel@protonmail.com>
2021-09-20 22:38:55 +0200

    feat(Webpack example 1): define entry and output

---
+++ Webpack/example1/config/webpack.config.dev.js
@@ -0,0 +1,13 @@
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: {
+    main: "./src/index.js",
+    // other-name: path-to-file => it renders another file
+  },
+  output: {
+    filename: "[name]-bundle.js",
+    path: path.resolve(__dirname, "..", "build"),
+  },
+};
