783bd9ae469420eaab2df7015ca092cb625ac2f6
danjarosz <jarosz.daniel@protonmail.com>
2021-09-25 09:33:49 +0200

    fix(Webpack project): babel config

--- Webpack/Project/config/webpack.config.js
+++ Webpack/Project/config/webpack.config.js
@@ -40,11 +40,13 @@ module.exports = {
         exclude: /node_modules/,
         options: {
           presets: [
-            "@babel/preset-env",
-            {
-              useBuiltIns: "usage",
-              corejs: "2.0.0",
-            },
+            [
+              "@babel/preset-env",
+              {
+                useBuiltIns: "usage",
+                corejs: "2.0.0",
+              },
+            ],
           ],
           plugins: ["@babel/plugin-proposal-class-properties"],
         },
