e488c918c0f15b6649d6b8440e3e925a30e53e37
danjarosz <jarosz.daniel@protonmail.com>
2021-09-25 09:04:12 +0200

    feat(Webpack project): basic babel configuration

--- Webpack/Project/config/webpack.config.prod.js
+++ Webpack/Project/config/webpack.config.prod.js
@@ -1,62 +1,67 @@
-const path = require('path');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require("path");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-  mode: 'production',
+  mode: "production",
   entry: {
-    main: './src/index.js',
+    main: "./src/index.js",
   },
   output: {
-    filename: 'js/[name]-[contenthash].js',
-    path: path.resolve(__dirname, '../', 'build')
+    filename: "js/[name]-[contenthash].js",
+    path: path.resolve(__dirname, "../", "build"),
   },
   module: {
     rules: [
       {
         test: /\.txt$/,
-        use: 'raw-loader'
+        use: "raw-loader",
       },
       {
         test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader']
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
       },
       {
         test: /\.(sass|scss)$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
       },
       {
         test: /\.(jpg|png|svg|gif|jpeg)$/,
-        use: [{
-          loader: 'file-loader',
-          options: {
-            name: '[name][contenthash:6].[ext]',
-            outputPath: 'images',
-          }
-        },
-        {
-          loader: 'image-webpack-loader',
-          options: {
-            mozjpeg: {
-              quality: 70,
-              progressive: true
-            }
-          }
-        }
-        ]
-
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[name][contenthash:6].[ext]",
+              outputPath: "images",
+            },
+          },
+          {
+            loader: "image-webpack-loader",
+            options: {
+              mozjpeg: {
+                quality: 70,
+                progressive: true,
+              },
+            },
+          },
+        ],
       },
-    ]
+      {
+        test: /\.js$/,
+        loader: "babel-loader",
+        exclude: /node_modules/,
+      },
+    ],
   },
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       template: "src/templates/template.html",
-      title: "nowa aplikacja"
+      title: "nowa aplikacja",
     }),
     new MiniCssExtractPlugin({
-      filename: '[name]-[contenthash].css'
-    })
-  ]
-}
+      filename: "[name]-[contenthash].css",
+    }),
+  ],
+};
