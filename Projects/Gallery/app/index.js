ca1dc02db515e334ea9761c7368b75e0c4db1920
danjarosz <jarosz.daniel@protonmail.com>
2021-09-27 19:50:59 +0200

    feat: index.html and index.js setup

--- Projects/Gallery/app/index.js
+++ Projects/Gallery/app/index.js
@@ -1,3 +1,38 @@
 import "../sass/style.scss";
 
-console.log("Boilerplate is working!");
+class Doggo {
+  constructor() {
+    this.apiUrl = "https://dog.ceo/api";
+    this.imgEl = document.querySelector(".featured-dog img");
+
+    this.init();
+  }
+
+  listBreeds = async () => {
+    return await fetch(`${this.apiUrl}/breeds/list/all`)
+      .then((res) => res.json())
+      .then((data) => data.message);
+  };
+
+  getRandomImage = async () => {
+    return await fetch(`${this.apiUrl}/breeds/image/random`)
+      .then((res) => res.json())
+      .then((data) => data.message);
+  };
+
+  getRandomImageByBreed = async (breed) => {
+    return await fetch(`${this.apiUrl}/breed/${breed}/images/random`)
+      .then((res) => res.json())
+      .then((data) => data.message);
+  };
+
+  init = () => {
+    this.getRandomImage().then((src) => this.imgEl.setAttribute("src", src));
+
+    this.listBreeds().then((breeds) => console.log(breeds));
+  };
+}
+
+window.addEventListener("DOMContentLoaded", () => {
+  new Doggo();
+});
