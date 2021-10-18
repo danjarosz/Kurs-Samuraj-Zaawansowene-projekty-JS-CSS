ec64d3f165ca8cdcf2531c7d571dafedbcffec8f
danjarosz <jarosz.daniel@protonmail.com>
2021-10-18 19:18:53 +0200

    feat(Canvas): stars animation

--- Projects/Canvas/src/js/Sky.js
+++ Projects/Canvas/src/js/Sky.js
@@ -14,6 +14,30 @@ class Sky {
     this.ctx.fillRect(0, 0, this.width, this.height);
   };
 
+  clearCanvas = () => {
+    this.ctx.fillStyle = "#000";
+    this.ctx.fillRect(0, 0, this.width, this.height);
+  };
+
+  generateStarts = (count) => {
+    let stars = [];
+
+    for (let i = 0; i < count; i++) {
+      const radius = Math.random() * 3 + 2;
+
+      stars.push({
+        x: Math.random() * this.width,
+        y: Math.random() * this.height,
+        radius,
+        orginalRadius: radius,
+        color: "#fff",
+        speed: Math.random() + 0.25,
+      });
+    }
+
+    this.stars = stars;
+  };
+
   drawStar = (star) => {
     // zapis stanu canvasa
     this.ctx.save();
@@ -34,12 +58,53 @@ class Sky {
     this.ctx.restore();
   };
 
+  drawStars = () => {
+    this.stars.forEach((star) => {
+      this.drawStar(star);
+    });
+  };
+
+  updateStars = () => {
+    this.stars.forEach((star) => {
+      star.x += star.speed;
+      star.y -= (star.speed * (this.width / 2 - star.x)) / 2800;
+      star.radius = star.orginalRadius * (Math.random() / 4 + 0.9);
+
+      if (star.x > this.width + 2 * star.radius) {
+        star.x = -2 * star.radius;
+      }
+    });
+  };
+
+  drawOverLayer = () => {
+    let gradient = this.ctx.createRadialGradient(
+      this.width / 2,
+      this.height / 2,
+      250,
+      this.width / 2,
+      this.height / 2,
+      this.width / 2
+    );
+    gradient.addColorStop(0, "rgba(0, 0, 0, 0)");
+    gradient.addColorStop(1, "rgba(0, 0, 0, 0.75");
+
+    this.ctx.fillStyle = gradient;
+    this.ctx.fillRect(0, 0, this.width, this.height);
+  };
+
   draw = () => {
+    this.clearCanvas();
+
+    this.drawStars();
+    this.updateStars();
+
+    this.drawOverLayer();
     window.requestAnimationFrame(this.draw);
   };
 
   run = () => {
     this.initCanvas();
+    this.generateStarts(500);
     this.draw();
   };
 }
