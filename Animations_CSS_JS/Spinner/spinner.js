d266135e66f1901c4f9388517e4b069174f33612
danjarosz <jarosz.daniel@protonmail.com>
2021-10-20 16:45:03 +0200

    feat(Spinner): using js

---
+++ Animations_CSS_JS/Spinner/spinner.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const spinner = document.querySelector(".v2");
+  const fps = (1000 / 60).toFixed();
+  let deg = 0;
+  const degChange = 6;
+
+  const rotate = () => {
+    deg += degChange;
+    spinner.style.transform = `rotate(${deg}deg)`;
+  };
+
+  setInterval(rotate, fps);
+});
